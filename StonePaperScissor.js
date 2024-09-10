c59c13944bf5b0623dd24086344f9a366b0273b2
Apoorvi S <2k22.csds.2211248@gmail.com>
2024-09-10 20:32:43 +0530

    completed js

--- StonePaperScissor.js
+++ StonePaperScissor.js
@@ -3,6 +3,10 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const userPara = document.querySelector("#user_score");
+const compPara = document.querySelector("#comp_score");
+
+
 const generateComputerChoice = () => {
     let options = ["rock","paper","scissor"];
     const randIdx = Math.floor(Math.random() * 3);
@@ -10,6 +14,21 @@ const generateComputerChoice = () => {
 }
 
 const drawGame = () => { console.log("Draw game");}
+
+const showWinner = (userWin) => {
+    if(userWin)
+    {
+        userScore++;
+        userPara.innerText = userScore;
+        console.log("user wins");
+    }
+    else
+    {
+        compScore++;
+        compPara.innerText = compScore;
+        console.log("computer wins");
+    }
+}
 
 const playGame = (userChoice) => {
     console.log("user choice: ",userChoice);
@@ -21,13 +40,33 @@ const playGame = (userChoice) => {
     {
         drawGame();
     }
+    else
+    {
+        let userWin = true;
+        if(userChoice=="rock")
+        {
+            userWin = compChoice=="paper" ? false : true;
+        }
+        else if (userChoice=="paper")
+        {
+            userWin = compChoice=="scissor" ? false : true;
+        }
+        else
+        {
+            userWin = compChoice=="rock" ? false : true;
+        }
+        showWinner(userWin);
+    }
 }
 
 choices.forEach((choice) => 
     {
         console.log(choice);
         choice.addEventListener("click", () => {
-            const userChoice = choice.getAttribute("id");
+            let userChoice = choice.getAttribute("id");
+            if(userChoice=="user_rock") userChoice = "rock";
+            else if(userChoice=="user_paper") userChoice = "paper";
+            else userChoice = "scissor";
             console.log("choice was clicked.",userChoice);
             playGame(userChoice);
         });
